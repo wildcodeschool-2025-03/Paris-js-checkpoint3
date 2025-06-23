d4fe722da0bf39fd662e80c4bab371cc90cbe808
jimmy <jimmy.balay97@gmail.com>
2025-06-23 10:33:52 +0200

    step2Finish

--- server/src/router.ts
+++ server/src/router.ts
@@ -4,6 +4,10 @@ const router = express.Router();
 
 /* ************************************************************************* */
 // Define Your API Routes Here
+
+import tileActions from "./modules/tile/tileActions";
+
+router.get("/api/tiles", tileActions.browse);
 /* ************************************************************************* */
 
 import boatActions from "./modules/boat/boatActions";
